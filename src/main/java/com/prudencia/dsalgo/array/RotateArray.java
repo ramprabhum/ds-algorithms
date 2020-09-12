425c01f49480927421d8ffc15978d253c64dd9d9
ramprabhum <ramprabhu.murugesan@gmail.com>
2020-09-12 21:55:31 +0530

    Setting up Arrays

--- src/main/java/com/prudencia/dsalgo/array/RotateArray.java
+++ src/main/java/com/prudencia/dsalgo/array/RotateArray.java
@@ -27,12 +27,5 @@ public class RotateArray {
             System.out.print(arr[i] + " ");
     }
 
-    // Driver program to test above functions
-    public static void main(String[] args)
-    {
-        RotateArray rotate = new RotateArray();
-        int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
-        rotate.leftRotate(arr, 2, 7);
-        rotate.printArray(arr, 7);
-    }
+
 }
