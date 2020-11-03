af663b2f4fd099fad7e971e937cb8dbee70f901a
ramprabhum <ramprabhu.murugesan@gmail.com>
2020-11-03 06:58:47 +0530

    doubly linked list

--- src/main/java/com/prudencia/dsalgo/linkedList/DoublyLinkedListOperations.java
+++ src/main/java/com/prudencia/dsalgo/linkedList/DoublyLinkedListOperations.java
@@ -47,6 +47,67 @@ public class DoublyLinkedListOperations {
 
     }
 
+
+    /* Given a node as prev_node, insert a new node after the given node */
+    public void InsertAfter(Node prev_Node, int new_data)
+    {
+
+        /*1. check if the given prev_node is NULL */
+        if (prev_Node == null) {
+            System.out.println("The given previous node cannot be NULL ");
+            return;
+        }
+
+        /* 2. allocate node
+         * 3. put in the data */
+        Node new_node = new Node(new_data);
+
+        /* 4. Make next of new node as next of prev_node */
+        new_node.next = prev_Node.next;
+
+        /* 5. Make the next of prev_node as new_node */
+        prev_Node.next = new_node;
+
+        /* 6. Make prev_node as previous of new_node */
+        new_node.prev = prev_Node;
+
+        /* 7. Change previous of new_node's next node */
+        if (new_node.next != null)
+            new_node.next.prev = new_node;
+    }
+
+    // Function to delete a node in a Doubly Linked List.
+    // head_ref --> pointer to head node pointer.
+    // del --> data of node to be deleted.
+    public void deleteNode(Node del)
+    {
+
+        // Base case
+        if (head == null || del == null) {
+            return;
+        }
+
+        // If node to be deleted is head node
+        if (head == del) {
+            head = del.next;
+        }
+
+        // Change next only if node to be deleted
+        // is NOT the last node
+        if (del.next != null) {
+            del.next.prev = del.prev;
+        }
+
+        // Change prev only if node to be deleted
+        // is NOT the first node
+        if (del.prev != null) {
+            del.prev.next = del.next;
+        }
+
+        // Finally, free the memory occupied by del
+        return;
+    }
+
     /* This function prints contents of doubly linked list starting from
     the given node */
     public void printList()
@@ -70,6 +131,9 @@ public class DoublyLinkedListOperations {
         doublyLinkedListOperations.append(6);
         doublyLinkedListOperations.push(3);
         doublyLinkedListOperations.append(5);
+        doublyLinkedListOperations.InsertAfter(doublyLinkedListOperations.head.next.next,11);
+        doublyLinkedListOperations.printList();
+        doublyLinkedListOperations.deleteNode(doublyLinkedListOperations.head.next.next.next.next);
         doublyLinkedListOperations.printList();
 
     }
