20ea3ad664ec5e7e6a724eefab59685f88c964d7
ramprabhum <ramprabhu.murugesan@gmail.com>
2022-10-29 16:50:10 +0530

    Detect Cycle & Dijakstra

---
+++ src/main/java/com/prudencia/dsalgo/operations/graph/DetectCycle.java
@@ -0,0 +1,99 @@
+package com.prudencia.dsalgo.operations.graph;
+
+
+
+// A Java Program to detect cycle in a graph
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+public class DetectCycle {
+
+        private final int V;
+        private final List<List<Integer>> adj;
+
+        public DetectCycle(int V)
+        {
+            this.V = V;
+            adj = new ArrayList<>(V);
+
+            for (int i = 0; i < V; i++)
+                adj.add(new LinkedList<>());
+        }
+
+        // Utility Function to check cycle in a Graph
+        private boolean isCyclicUtil(int i, boolean[] visited,
+                                     boolean[] recStack)
+        {
+
+            // Mark the current node as visited and
+            // part of recursion stack
+            if (recStack[i])
+                return true;
+
+            if (visited[i])
+                return false;
+
+            visited[i] = true;
+
+            recStack[i] = true;
+            List<Integer> children = adj.get(i);
+
+            for (Integer c: children)
+                if (isCyclicUtil(c, visited, recStack))
+                    return true;
+
+            recStack[i] = false;
+
+            return false;
+        }
+
+        private void addEdge(int source, int dest) {
+            adj.get(source).add(dest);
+        }
+
+        // Returns true if the graph contains a
+        // cycle, else false.
+        // This function is a variation of DFS() in
+        // https://www.cdn.geeksforgeeks.org/archives/18212
+        private boolean isCyclic()
+        {
+            // Mark all the vertices as not visited and
+            // not part of recursion stack
+            boolean[] visited = new boolean[V];
+            boolean[] recStack = new boolean[V];
+
+
+            // Call the recursive helper function to
+            // detect cycle in different DFS trees
+            for (int i = 0; i < V; i++)
+                if (isCyclicUtil(i, visited, recStack)){
+                    System.out.println(Arrays.toString(visited));
+                    System.out.println(Arrays.toString(recStack));
+                    System.out.println("*******");
+                    return true;
+                }
+
+
+            return false;
+        }
+
+        // Driver code
+        public static void main(String[] args)
+        {
+            DetectCycle graph = new DetectCycle(4);
+            graph.addEdge(0, 1);
+            graph.addEdge(0, 2);
+            graph.addEdge(1, 2);
+            graph.addEdge(2, 0);
+            graph.addEdge(2, 3);
+            graph.addEdge(3, 3);
+
+            if(graph.isCyclic())
+                System.out.println("Graph contains cycle");
+            else
+                System.out.println("Graph doesn't "
+                        + "contain cycle");
+        }
+    }
+
